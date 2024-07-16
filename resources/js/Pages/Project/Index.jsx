12849399e3de625860e5989f6f149633c0053813
George 'SurvivalX' Christmas <goshaz7c@icloud.com>
2024-07-16 23:49:22 +0300

    pagination created in react-> next ( disable scroll to the top when switching page )

---
+++ resources/js/Pages/Project/Index.jsx
@@ -0,0 +1,59 @@
+import { Head, Link } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import Pagination from '@/Components/Pagination';
+
+
+export default function Index({ auth, projects }) {
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>}>
+
+      <Head title="Projects" />
+
+      <div className="py-12 dark:br-gray-600">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white dark:bg-gray-800 border-2-red overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900">
+
+              <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+                <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500'>
+                  <tr className='text-nowrap'>
+                    <th className='px-3 py-2'>ID</th>
+                    <th className='px-3 py-2'>Image</th>
+                    <th className='px-3 py-2'>Name</th>
+                    <th className='px-3 py-2'>Status</th>
+                    <th className='px-3 py-2'>Create Date</th>
+                    <th className='px-3 py-2'>Due Date</th>
+                    <th className='px-3 py-2'>Created By</th>
+                    <th className='px-3 py-2 text-right'>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {projects.data.map((project) => (
+                    <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
+                      <td className='px-3 py-3'>{project.id}</td>
+                      <td className='px-3 py-3'>
+                        <img src={project.image_path} alt="" className='w-16' />
+                      </td>
+                      <td className='px-3 py-3'>{project.name}</td>
+                      <td className='px-3 py-3'>{project.status}</td>
+                      <td className='px-3 py-3 text-nowrap'>{project.created_at}</td>
+                      <td className='px-3 py-3 text-nowrap'>{project.due_date}</td>
+                      <td className='px-3 py-3'>{project.createdBy.name}</td>
+                      <td className='px-3 py-3'>
+                        <Link href={route('project.edit', project.id)} className='font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1'>Edit</Link>
+                        <Link href={route('project.destroy', project.id)} className='font-medium text-red-600 dark:text-red-500 hover:underline mx-1'>Delete</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <Pagination links={projects.meta.links} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  )
+}
