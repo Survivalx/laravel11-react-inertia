cf249efc68486860791774e727fd5232090f4032
Survivalx <goshaz7c@icloud.com>
2024-07-17 14:53:37 +0300

    filtering projects started

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -5,6 +5,7 @@ export default function Pagination({ links }) {
     <nav className="text-center mt-4">
       {links.map(link => (
         <Link
+        preserveScroll
         href={link.url || ""}
         key={link.label}
         className={"inline-block py-2 px-3 rounded-lg text-gray-200 text-xs " +
--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,13 +1,31 @@
-import { Head, Link } from '@inertiajs/react';
+import { Head, Link, router } from '@inertiajs/react';
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import Pagination from '@/Components/Pagination';
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from '@/constants.jsx';
+import TextInput from '@/Components/TextInput';
+import SelectInput from '@/Components/SelectInput';
 
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+  queryParams = queryParams || {}
+  const searchFieldChanged = (name, value) => {
+    if (value) {
+      queryParams[name] = value
+    } else {
+      delete queryParams[name]
+    }
+    router.get(route('project.index'), queryParams)
+  }
+  const onKeyPress = (name, e) => {
+    if (e.key !== 'Enter') return;
+
+    searchFieldChanged(name, e.target.value)
+  }
+
   return (
     <AuthenticatedLayout
       user={auth.user}
-      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>}>
+      header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-100 leading-tight">Projects</h2>}>
 
       <Head title="Projects" />
 
@@ -29,15 +47,46 @@ export default function Index({ auth, projects }) {
                     <th className='px-3 py-2 text-right'>Actions</th>
                   </tr>
                 </thead>
+                <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500'>
+                  <tr className='text-nowrap'>
+                    <th className='px-3 py-2'></th>
+                    <th className='px-3 py-2'></th>
+                    <th className='px-3 py-2'>
+                      <TextInput
+                        className="w-full"
+                        placeholder="Project Name"
+                        onBlur={e => searchFieldChanged('name', e.target.value)}
+                        onKeyPress={e => onKeyPress('name', e)} />
+                    </th>
+                    <th className='px-3 py-2'>
+                      <SelectInput className="w-full"
+                        onChange={(e) => searchFieldChanged("status", e.target.value)}>
+                        <option value="">Select Status</option>
+                        <option value="pending">Pending</option>
+                        <option value="in_progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </SelectInput>
+                    </th>
+                    <th className='px-3 py-2'></th>
+                    <th className='px-3 py-2'></th>
+                    <th className='px-3 py-2'></th>
+                    <th className='px-3 py-2 text-right'></th>
+                  </tr>
+                </thead>
                 <tbody>
                   {projects.data.map((project) => (
-                    <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
+                    <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700' key={project.id}>
                       <td className='px-3 py-3'>{project.id}</td>
                       <td className='px-3 py-3'>
                         <img src={project.image_path} alt="" className='w-16' />
                       </td>
                       <td className='px-3 py-3'>{project.name}</td>
-                      <td className='px-3 py-3'>{project.status}</td>
+                      <td className='px-3 py-3'>
+                        <span className={
+                          "px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]
+                        }>
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span></td>
                       <td className='px-3 py-3 text-nowrap'>{project.created_at}</td>
                       <td className='px-3 py-3 text-nowrap'>{project.due_date}</td>
                       <td className='px-3 py-3'>{project.createdBy.name}</td>
